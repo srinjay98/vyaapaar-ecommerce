6f16b679d3d44dee75e21c69909e1ecb78d16fe3
srinjay98 <Srinjay8024@gmail.com>
2026-06-03 17:35:31 +0530

    Add category debugging and product form checks

--- vyaapaar/products/models.py
+++ vyaapaar/products/models.py
@@ -10,9 +10,22 @@ from django.core.validators import MinValueValidator
 class Category(models.Model):
 
     name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
+
+    slug = models.SlugField(
+        unique=True,
+        blank=True
+    )
+
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
+
         return self.name
 
 
